2f796c91d79ec507d0629ded60902008e53d9352
hogshead-revival-widow <67462744+hogshead-revival-widow@users.noreply.github.com>
2021-06-18 00:42:45 +0200

    Entfernung Provider; Entfernung reachable-Status

--- src/options.js
+++ src/options.js
@@ -1,9 +1,5 @@
-import { DEFAULT_PROVIDER, DEFAULT_URL } from './const.js'
-import { handleIsReachable } from './utils.js'
-
 const defaults = {
-  installDate: null,
-  provider: DEFAULT_PROVIDER
+  installDate: null
 }
 
 function restore () {
@@ -13,28 +9,12 @@ function restore () {
       browser.storage.sync.set({
         installDate: new Date().getTime()
       })
-      browser.storage.sync.set({
-        provider: DEFAULT_PROVIDER
-      })
-    }
-  })
-
-  handleIsReachable(window, DEFAULT_URL, function (isReachable) {
-    const statusEle = document.querySelector('#verbindung')
-    if (!isReachable) {
-      statusEle.classList.remove('is-success')
-      statusEle.classList.add('is-danger')
-      statusEle.innerText = 'offline'
-    } else {
-      statusEle.classList.remove('is-danger')
-      statusEle.classList.add('is-success')
-      statusEle.innerText = 'online'
     }
   })
 
   window.fetch('/manifest.json').then(response => response.json())
     .then(data => {
-      const domains = data.content_scripts[0].matches.map(url => url.replace('https://', '').replace('http://', '').replace('/*', ''))
+      const domains = data.content_scripts[0].matches.map(url => url.replace('https://', '').replace('*://','').replace('http://', '').replace('/*', ''))
       const ul = document.getElementById('newssites')
       domains.forEach(domain => {
         const li = document.createElement('li')
