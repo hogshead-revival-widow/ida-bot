f6aec70a1dbd5ddb29a595001f7658c0ace38180
hogshead-revival-widow <67462744+hogshead-revival-widow@users.noreply.github.com>
2021-06-18 00:52:49 +0200

    Neu: Fallback

--- src/sources.js
+++ src/sources.js
@@ -1,48 +1,76 @@
-import { DEFAULT_URL } from './const.js'
+import { DEFAULT_URL, FALLBACK_URL, INFO_FAILED_TO_FIND_CONTENT } from './const.js'
 import {
   MSG_LOGIN_SELECTION, MSG_LOGIN_ACCESS, MSG_START_SEARCH,
-  MSG_ARTICLE, MSG_ARTICLE_FAILURE
+  MSG_ARTICLE, MSG_WAIT, MSG_INTERACTION_SSO
 } from './ui.js'
 
 export default {
-  pan: {
-    loggedIn: '#retr_std_inp_sbegr',
-    start: DEFAULT_URL,
-    defaultParams: {
-      domain: DEFAULT_URL
+    'pan': {
+      loggedIn: '#retr_buttonSuchen',
+      start: DEFAULT_URL,
+      defaultParams: {
+        domain: DEFAULT_URL
+      },
+      login: [
+          [
+            { message: MSG_WAIT},
+            { wait: 600000} // Wird abgebrochen, sobald `loggedIn`gefunden; Zeit für die ganzen Umleitungen
+          ]
+      ],
+      search: [
+        [
+          { message: MSG_START_SEARCH },
+          { click: '#retr_buttonZuruecksetzen' },
+          { fill: { selector: '#retr_std_inp_sbegr', isQuery: true } },
+          { fill: { selector: '#retr_inp_quell', isSource: true } },
+          { click: '#id_verknuep_phrase' },
+          // ohne Datumsbeschränkung suchen, Voreinstellung: 1 Jahr
+          { click: '#retr_datumZuruecksetzen' },
+          { click: '#retr_buttonSuchen' }
+        ],
+        [
+          { message: MSG_ARTICLE },
+          { click: '.sh_a_hit_access_ok' },
+          { failOnMissing: '.sh_a_hit_access_ok', failure: INFO_FAILED_TO_FIND_CONTENT }
+        ],
+  
+        [
+          { wait: 1000},
+          { extract: '#dt_dv_atext' }
+        ]
+      ]
     },
-    login: [
-      [
-        { message: MSG_LOGIN_SELECTION },
-        { click: '#content a[href*="100"]' }
+    'fallback': {
+      loggedIn: '#retr_std_inp_sbegr',
+      start: FALLBACK_URL,
+      defaultParams: {
+        domain: FALLBACK_URL
+      },
+      login: [
+        [
+          { message: MSG_INTERACTION_SSO},
+          { wait: 600000} // Zehn Minuten Zeit für's manuelle Einloggen (wird abgebrochen, wenn Login erfolgreich)
+        ],
       ],
-      [
-        { wait: 600 }
-      ],
-      [
-        { message: MSG_LOGIN_ACCESS },
-        { click: '.logon_gast .logon_anmeldebutton a' }
+      search: [
+        [
+          { message: MSG_START_SEARCH },
+          { click: '#retr_buttonZuruecksetzen' },
+          { fill: { selector: '#retr_std_inp_sbegr', isQuery: true } },
+          { fill: { selector: '#retr_inp_quell', isSource: true } },
+          { click: '#id_verknuep_phrase' },
+          // ohne Datumsbeschränkung suchen, Voreinstellung: 1 Jahr
+          { click: '#retr_datumZuruecksetzen' },
+          { click: '#retr_buttonSuchen' }
+        ],
+        [
+          { message: MSG_ARTICLE },
+          { failOnMissing: '.sh_a_hit_access_ok', failure: INFO_FAILED_TO_FIND_CONTENT },
+          { click: '.sh_a_hit_access_ok' }
+        ],
+        [
+          { extract: '#dt_dv_atext' }
+        ]
       ]
-    ],
-    search: [
-      [
-        { message: MSG_START_SEARCH },
-        { click: '#retr_buttonZuruecksetzen' },
-        { fill: { selector: '#retr_std_inp_sbegr', isQuery: true } },
-        { fill: { selector: '#retr_inp_quell', isSource: true } },
-        { click: '#id_verknuep_phrase' },
-        // ohne Datumsbeschränkung suchen, Voreinstellung: 1 Jahr
-        { click: '#retr_datumZuruecksetzen' },
-        { click: '#retr_buttonSuchen' }
-      ],
-      [
-        { message: MSG_ARTICLE },
-        { failOnMissing: '.sh_a_hit_access_ok', failure: MSG_ARTICLE_FAILURE },
-        { click: '.sh_a_hit_access_ok' }
-      ],
-      [
-        { extract: '#dt_dv_atext', getPDF: true }
-      ]
-    ]
   }
 }
