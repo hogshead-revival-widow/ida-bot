cdea18dd822a48eed26ce9283f40566727783e31
hogshead-revival-widow <67462744+hogshead-revival-widow@users.noreply.github.com>
2021-06-18 00:41:08 +0200

    FALLBACK_URL, Fehlerstatus'

--- src/const.js
+++ src/const.js
@@ -4,6 +4,13 @@ export const GOTOTAB_MESSAGE = 'gototab'
 export const SUCCES_MESSAGE = 'success'
 export const FAILED_MESSAGE = 'failed'
 export const STATUS_MESSAGE = 'status'
-export const DEFAULT_PROVIDER = 'S-PAN'
-export const DEFAULT_URL = 'http://127.0.0.1:5000/test/start' 
+
+export const DEFAULT_URL = 'http://127.0.0.1:5000/test/start'
+export const FALLBACK_URL = 'https://127.0.0.1:5000/fallback/start''
+export const FALLBACK_SOURCE = 'fallback'
+export const DEFAULT_SOURCE = 'pan'
+
+// Fehler
 export const ERROR_UNKNOWN_TYPE = 'Unbekannter Typ'
+export const ERROR_UNKNOWN_SOURCE = 'Unbekannte Aktion in source'
+export const INFO_FAILED_TO_FIND_CONTENT = 'Artikel nicht gefunden'
